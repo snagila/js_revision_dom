3a40f0c31b5ac2d791d6b506d978927fab212e28
Sachin Nagila <sachinnagila2053@gmail.com>
2024-05-04 11:48:12 +1000

    variables pratice

---
+++ script.js
@@ -0,0 +1,16 @@
+// using variables
+const a = 5;
+let b = 5;
+
+// using arithmetic operators
+const addition = a + b;
+console.log(addition);
+
+var multiplication = a * b;
+console.log(multiplication);
+
+let division = 10 / a;
+console.log(division);
+
+// using textcontent for output
+document.getElementById("clickme").textContent = multiplication;
